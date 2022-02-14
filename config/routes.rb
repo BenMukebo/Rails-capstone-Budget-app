81f483071ef413691951ea45690e9c96d65cc87c
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-14 21:35:31 +0200

    Config all necessary routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  get 'records/new'
-  get 'records/create'
-  get 'records/update'
-  get 'records/destroy'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'splash/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'splash#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :records, only: [:new, :create, :update, :destroy]
+  end
 end
