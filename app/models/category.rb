b1ee53965ad357b0c0f7b64008966ad5f699c329
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-15 21:12:32 +0200

    Add some validations for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :category_transactions
-  has_many :transactions
+  has_many :category_transactions, dependent: :destroy
+  has_many :transactions, through: :category_transactions
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :icon, presence: true, length: { maximum: 250 }
+
+  def recent_transactions
+    transactions.order(created_at: :DESC).limit(3)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  has_many :category_transactions
-  has_many :categories
+  has_many :category_transactions, dependent: :destroy
+  has_many :categories, through: :category_transactions
+
+  validates :name, presence: true
+  validates :ammount, numericality: { greater_than: 0 }
 end
